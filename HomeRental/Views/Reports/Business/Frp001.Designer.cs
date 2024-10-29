7c88148269f717d2add7dea2f770b87f2ff48932
duynv <duynguyen.19x@gmail.com>
2024-10-29 17:18:42 +0700

    Commit

--- HomeRental/Views/Reports/Business/Frp001.Designer.cs
+++ HomeRental/Views/Reports/Business/Frp001.Designer.cs
@@ -32,30 +32,52 @@
             this.BottomMargin = new DevExpress.XtraReports.UI.BottomMarginBand();
             this.Detail = new DevExpress.XtraReports.UI.DetailBand();
             this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
+            this.txtTime = new DevExpress.XtraReports.UI.XRLabel();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             // 
             // TopMargin
             // 
+            this.TopMargin.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.txtTime,
+            this.xrLabel1});
+            this.TopMargin.HeightF = 118.75F;
             this.TopMargin.Name = "TopMargin";
             // 
             // BottomMargin
             // 
+            this.BottomMargin.HeightF = 40.27778F;
             this.BottomMargin.Name = "BottomMargin";
             // 
             // Detail
             // 
-            this.Detail.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
-            this.xrLabel1});
+            this.Detail.HeightF = 151.3889F;
             this.Detail.Name = "Detail";
             // 
             // xrLabel1
             // 
-            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(183.3333F, 28.77778F);
+            this.xrLabel1.Font = new DevExpress.Drawing.DXFont("Times New Roman", 16F, DevExpress.Drawing.DXFontStyle.Bold);
+            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(0F, 31.55556F);
             this.xrLabel1.Multiline = true;
             this.xrLabel1.Name = "xrLabel1";
             this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F);
-            this.xrLabel1.SizeF = new System.Drawing.SizeF(304.8611F, 23F);
-            this.xrLabel1.Text = "adsadadadkjasdkljas";
+            this.xrLabel1.SizeF = new System.Drawing.SizeF(725.9999F, 32.72223F);
+            this.xrLabel1.StylePriority.UseFont = false;
+            this.xrLabel1.StylePriority.UseTextAlignment = false;
+            this.xrLabel1.Text = "BÁO CÁO DOANH THU CHO THUÊ NHÀ";
+            this.xrLabel1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
+            // 
+            // txtTime
+            // 
+            this.txtTime.Font = new DevExpress.Drawing.DXFont("Times New Roman", 9.75F, DevExpress.Drawing.DXFontStyle.Italic);
+            this.txtTime.LocationFloat = new DevExpress.Utils.PointFloat(0F, 64.27778F);
+            this.txtTime.Multiline = true;
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F);
+            this.txtTime.SizeF = new System.Drawing.SizeF(725.9999F, 23F);
+            this.txtTime.StylePriority.UseFont = false;
+            this.txtTime.StylePriority.UseTextAlignment = false;
+            this.txtTime.Text = "txtTime";
+            this.txtTime.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
             // 
             // Frp001
             // 
@@ -64,6 +86,7 @@
             this.BottomMargin,
             this.Detail});
             this.Font = new DevExpress.Drawing.DXFont("Arial", 9.75F);
+            this.Margins = new DevExpress.Drawing.DXMargins(62F, 62F, 118.75F, 40.27778F);
             this.Version = "24.1";
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
 
@@ -75,5 +98,6 @@
         private DevExpress.XtraReports.UI.BottomMarginBand BottomMargin;
         private DevExpress.XtraReports.UI.DetailBand Detail;
         private DevExpress.XtraReports.UI.XRLabel xrLabel1;
+        private DevExpress.XtraReports.UI.XRLabel txtTime;
     }
 }
--- HomeRental/Views/Reports/Business/Frp001.cs
+++ HomeRental/Views/Reports/Business/Frp001.cs
@@ -13,5 +13,9 @@ namespace HomeRental.Views.Reports.Business
             InitializeComponent();
         }
 
+        public Frp001(DateTime fromDate, DateTime toDate)
+        {
+            txtTime.Text = string.Format("Từ ngày: {0} - đến ngày: {1}", fromDate.ToString("dd/MM/yyyy"), toDate.ToString("dd/MM/yyyy"));
+        }
     }
 }
